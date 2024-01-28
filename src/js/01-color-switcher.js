9320b456a74689ce25013ef4821818f364661615
EugenAd <bosmaneugen@gmail.com>
2024-01-28 13:25:54 +0200

    Finished task 01-color-switcer

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
-
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+let color = '';
+const body = document.querySelector('body');
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+let timeInterval;
+const handleStart = () => {
+  color = getRandomHexColor();
+  body.style.backgroundColor = color;
+};
+startButton.addEventListener('click', () => {
+  timeInterval = setInterval(handleStart, 1000);
+  startButton.disabled = true;
+});
+stopButton.addEventListener('click', () => {
+  clearInterval(timeInterval);
+  startButton.disabled = false;
+});
